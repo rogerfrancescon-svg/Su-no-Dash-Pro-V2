ceceaa31f2bbe10e2eda3ca126db36fae1d19d13
rogerfrancescon-svg <rogerfrancescon@gmail.com>
2026-08-08 13:58:23 -0300

    correção github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/',
+  base: process.env.GITHUB_ACTIONS ? '' : '/',
   build: {
     outDir: 'dist',
     chunkSizeWarningLimit: 1500,
